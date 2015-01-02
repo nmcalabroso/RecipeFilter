28449f92a28e72366e9b402cd0ad2f0cf203e738
Mireya Andres <mireyagenandres@gmail.com>
2015-01-02 22:48:50 +0800

    Adds sample pages.

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('search', { path: "/"});
+  this.route('results');
+  this.route('recipe');
   this.route('login');
 });
 
