1b694d8e84878444575df8dec693bff04e6b5051
Mireya Andres <mireyagenandres@gmail.com>
2015-01-02 23:42:05 +0800

    Adds fixture data for /results and /recipes. Setup for API.
    Adds fixture data for /results and /recipes. Setup for API.
    
    Adds fixture data for results.
    
    Adds fixture data for recipe.
    
    Login link.
    
    Top padding.

---
+++ frontend/app/adapters/application.js
@@ -0,0 +1,9 @@
+// import ApplicationAdapter from './application';
+
+// export default ApplicationAdapter.extend({
+// });
+
+import DS from 'ember-data';
+ 
+export default DS.FixtureAdapter.extend({}); 
+
--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,15 +1,18 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
-  this.resource('search', { path: "/"});
-  this.route('results');
-  this.route('recipe');
-  this.route('login');
+  this.resource("search", {
+    path: "/"
+  });
+
+  this.route("results");
+  this.route("recipe");
+  this.route("login");
 });
 
 export default Router;
