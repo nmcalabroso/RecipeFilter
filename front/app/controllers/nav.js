6356d5342fdea57625983f52d9322bdbdff43a14
Mireya Andres <mireyagenandres@gmail.com>
2015-01-08 04:44:46 +0800

    Merges nav and nav-modals.

--- front/app/controllers/nav.js
+++ front/app/controllers/nav.js
@@ -2,6 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    createSession: function(){
+      var self = this;
+      var login = this.get('username');
+      var password = this.get('password');
+
+      if(!Ember.isEmpty(login) && !Ember.isEmpty(password)){
+        $.post(
+            '/api/users/login',
+            {"user_session": {"login":login, "password":password}},
+            function(data){
+              $('#login').modal('hide');
+              self.transitionToRoute("/recipes");
+          });
+      }
+    },
+    
     destroySession: function(){
       var self = this;
       if(typeof $.cookie("user_credentials") !== undefined){
@@ -14,5 +30,6 @@ export default Ember.Controller.extend({
        }); 
       }
     }
-  }
+  },
+
 });
