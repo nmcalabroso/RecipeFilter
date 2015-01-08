c6c596f98c6eb9538594555c8baa8dad9a046405
Mireya Andres <mireyagenandres@gmail.com>
2015-01-08 17:11:19 +0800

    Updates config/environment.js

--- front/app/models/recipe.js
+++ front/app/models/recipe.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  description: DS.attr('text'),
+  description: DS.attr(),
 });
--- front/app/routes/recipes/index.js
+++ front/app/routes/recipes/index.js
@@ -3,12 +3,37 @@ import ajax from 'ic-ajax';
 
 export default Ember.Route.extend({ 
   model: function(){
-    return ajax({
+    var data = ajax({
       url: 'api/recipes/',
       type: 'get'
-    }).then(function(data){
-      console.log(data);
-      return data;
     });
+
+
+    // .then(function(data){
+    //   console.log(data);
+    //   return data;
+    // });
+
+    // data.forEach(function(sample){
+    //   console.log(sample);
+    // });
+
+
+    // data.each(function(sample){
+    //   console.log(sample);
+      // var recipe = this.store.createRecord('recipe', {
+      //   name: sample.name,
+      //   description: sample.description
+      // });
+
+      // recipe.save();
+    // });      
+
+
+    // return this.store.find('recipe');
+    return data;
+
+
+
   }
 });
